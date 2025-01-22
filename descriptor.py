df57e543288adc48502ed961a91a8f744d3ba6b1
Houssam Eddine Imhiouach <141267544+HoussamImhiouach@users.noreply.github.com>
2025-01-21 20:53:52 -0500

    Update descriptor.py with the correct descriptor import

--- descriptor.py
+++ descriptor.py
@@ -2,7 +2,7 @@
 import cv2
 import numpy as np
 from skimage.feature import graycomatrix, graycoprops
-from BiT import bio_taxo
+from bitdesc import bio_taxo
 
 def glcm(image):
     co_matrix = graycomatrix(image, [1], [0], symmetric=True, normed=True)
